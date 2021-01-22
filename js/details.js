7d77de1962a372604ead87ddda512c02650e344a
epicrazor <jimmyrazor@yahoo.com>
2021-01-22 13:53:55 +0200

    project-mobile-full-responsive

---
+++ js/details.js
@@ -0,0 +1,17 @@
+var a = document.querySelector("details");
+
+var b = document.getElementsByTagName("details")[1];
+var c = document.getElementsByTagName("details")[2];
+// var atts = b.attributes;
+
+if (window.screen.width >= 768) {
+    a.setAttribute("open", "open");
+    b.setAttribute("open", "open");
+    c.setAttribute("open", "open");
+  }
+  
+
+
+
+
+
