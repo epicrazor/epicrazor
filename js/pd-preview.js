f455f3e7bb825bcba4781773e919075d3ef1919a
epicrazor <jimmyrazor@yahoo.com>
2021-01-25 19:52:44 +0200

    pdp added

---
+++ js/pd-preview.js
@@ -0,0 +1,30 @@
+function openModal() {
+    document.getElementByClass("product-preview").style.display = "block";
+  }
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("showProduct");
+    var dots = document.getElementsByClassName("prv");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "initial";
+    dots[slideIndex-1].className += " active";
+  }
