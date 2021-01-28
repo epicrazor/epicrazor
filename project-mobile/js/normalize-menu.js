aa457e47a31c0cf544619fd8a39f76f30516bad4
epicrazor <jimmyrazor@yahoo.com>
2021-01-28 05:43:37 +0200

    dropdowns

---
+++ project-mobile/js/normalize-menu.js
@@ -0,0 +1,27 @@
+if (767 < window.screen.width) {  
+  document.getElementById("menu").classList.remove('mobile-menu');
+
+//   var btn = document.getElementById("dropbtn");
+//   btn.setAttribute("onmouseover","openMega(event)");
+//   btn.setAttribute("onmouseout","closeMega(event)");
+ 
+  
+// function openMega(event){
+//   var unhide = document.getElementById("megaMenu");
+//   unhide.classList.remove("hide");
+//   console.log("open");
+ 
+
+// }
+
+// function closeMega(event){
+//   var hide = document.getElementById("megaMenu");
+//   hide.classList.add("hide");
+//   console.log("close");
+// }
+
+
+
+
+
+}
